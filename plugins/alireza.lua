2db5fb290e1c4864a1e299c7a4b5a2257121ada5
sahebankherad <arsalangorbanzade2013@gmail.com>
2016-01-10 13:43:03 +0330

    Create alireza.lua

---
+++ plugins/alireza.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return " تخس و اتکر , خطرناک ولی مهربان , محبوب دلها  و پشتیبان عالی با ای دی @alireza_creed"
+  end
+return {
+  description = "alireza creed",
+  usage = "say alireza and bot will return",
+  patterns = {
+    "^Alireza$",
+    "^alireza$",
+  },
+  run = run
+}
+end
