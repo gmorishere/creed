765f7c347499ab5ba1979e88711d78b1fa375d97
sahebankherad <arsalangorbanzade2013@gmail.com>
2016-01-10 13:35:33 +0330

    Update and rename creeds.lua to aria.lua

---
+++ plugins/aria.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return " آریای کبیر , جیگر ارسلان , محبوب دلها , تنهای شب ها و ادمین بات ها : @aria_creed "
+  end
+return {
+  description = "Creed developer",
+  usage = "say aria and bot will return",
+  patterns = {
+    "^Aria$",
+    "^aria$",
+  },
+  run = run
+}
+end
