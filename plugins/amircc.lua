d1ddb02b753153c8ade6406d26d1cfad73955334
sahebankherad <arsalangorbanzade2013@gmail.com>
2016-01-10 13:37:41 +0330

    Create amircc.lua

---
+++ plugins/amircc.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return "معروف به امیر زاخار , فول انرژی , خستگی ناپذیر و هکری گمنام با ای دی @amircc_creed  "
+  end
+return {
+  description = "Creed developer",
+  usage = "say Amircc and bot will return",
+  patterns = {
+    "^amircc$",
+    "^Amircc$",
+  },
+  run = run
+}
+end
