2567bf5e8f7ee82b91c62cf3979a35419303bd50
sahebankherad <arsalangorbanzade2013@gmail.com>
2016-01-10 13:33:29 +0330

    Create nerkh.lua

---
+++ plugins/nerkh.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return " نرخ هر گروه دو تومن شارژ همراه اول ماهانه هست . با پشتیبانی عالی و 2$
+  end
+return {
+  description = "says how to but a Group",
+  usage = "nerkh : Cost of the Groups and how to buy it from Creed bot",
+  patterns = {
+    "^nerkh$",
+    "^Nerkh$",
+    "^gp$",
+    "^group$",
+  },
+  run = run
+}
+end
+
