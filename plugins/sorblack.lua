8b16fd08ab286b53549314b5ef2a4089d37bdac5
sahebankherad <arsalangorbanzade2013@gmail.com>
2016-01-10 13:40:52 +0330

    Create sorblack.lua

---
+++ plugins/sorblack.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return "  کنجکاو  و عاقل , مهربون و شاد دل , همیشه حاضر , بردبار و ناظر بر ربات با ای دی @sorblack_creed"
+  end
+return {
+  description = "Creed developer",
+  usage = "say Amircc and bot will return",
+  patterns = {
+    "^surush$",
+    "^soroosh$",
+    "^soorosh$",
+    "^Soorosh$",
+    "^surosh$",
+    "^Soorosh$",
+    "^Surush$",
+    "^Sorblack$",
+    "^sorblack$",
+  },
+  run = run
+}
+end
